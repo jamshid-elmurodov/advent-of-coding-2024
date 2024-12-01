edca66f2b511ceb7bc064e26ad161dad589597cc
Jamshid <elmurodovjamshid38@gmail.com>
2024-12-01 12:40:00 +0500

    day1

--- src/day1/Problem1.java
+++ src/day1/Problem1.java
@@ -6,76 +6,48 @@ import java.nio.file.Path;
 import java.util.*;
 
 public class Problem1 {
-    static int part1(){
-        try {
-            List<String> lines = Files.readAllLines(Path.of("src/day1/input.txt"));
+    static int part1(List<Integer> list1, List<Integer> list2){
+        Collections.sort(list1);
+        Collections.sort(list2);
 
-            List<Integer> list1 = new ArrayList<>();
-            List<Integer> list2 = new ArrayList<>();
+        int sum = 0;
 
-            for (String s : lines) {
-                String[] split = s.split("\\s+");
-
-                System.out.println(s);
-
-                System.out.println(Arrays.toString(split));
-
-                list1.add(Integer.valueOf(split[0]));
-                list2.add(Integer.valueOf(split[1]));
-            }
-
-            Collections.sort(list1);
-            Collections.sort(list2);
-
-            int sum = 0;
-
-            for (int i = 0; i < list1.size(); i++) {
-                sum += Math.abs(list1.get(i) - list2.get(i));
-            }
-
-            return sum;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        for (int i = 0; i < list1.size(); i++) {
+            sum += Math.abs(list1.get(i) - list2.get(i));
         }
+
+        return sum;
     }
 
-    static int part2(){
-        try {
-            List<String> lines = Files.readAllLines(Path.of("src/day1/input.txt"));
+    static int part2(List<Integer> list1, List<Integer> list2){
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-            List<Integer> list1 = new ArrayList<>();
-            List<Integer> list2 = new ArrayList<>();
-
-            for (String s : lines) {
-                String[] split = s.split("\\s+");
-
-                System.out.println(s);
-
-                System.out.println(Arrays.toString(split));
-
-                list1.add(Integer.valueOf(split[0]));
-                list2.add(Integer.valueOf(split[1]));
-            }
-
-            HashMap<Integer, Integer> map = new HashMap<>();
-
-            for (Integer i : list2) {
-                map.put(i, map.getOrDefault(i, 0) + 1);
-            }
-
-            int sum = 0;
-
-            for (Integer i : list1) {
-                sum += map.getOrDefault(i, 0) * i;
-            }
-
-            return sum;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        for (Integer i : list2) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
         }
+
+        int sum = 0;
+
+        for (Integer i : list1) {
+            sum += map.getOrDefault(i, 0) * i;
+        }
+
+        return sum;
     }
 
-    public static void main(String[] args) {
-        System.out.println(part2());
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(Path.of("src/day1/input.txt"));
+
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+
+        for (String s : lines) {
+            String[] split = s.split("\\s+");
+
+            list1.add(Integer.valueOf(split[0]));
+            list2.add(Integer.valueOf(split[1]));
+        }
+
+        System.out.println(part2(list1, list2));
     }
 }
